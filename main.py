539b175ec521c70601b6ee0c56d135e5f8d5e069
AlexisComix <thecomixguy@hotmail.com>
2022-06-17 14:59:40 +0100

    Fix Issue #1

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ HEIGHT = 650
 SCREEN_SIZE = (WIDTH, HEIGHT)
 
 pygame.font.init()
-COMICSANSMS = pygame.font.SysFont("comicsansms", 20)
+COMICSANSMS = pygame.font.SysFont("comicsansms", 15)
 
 
 pygame.init()
@@ -62,14 +62,14 @@ def get_mines(number_of_mines) -> list:
     grid_width = WIDTH // BLOCKSIZE
     grid_height = GRID_HEIGHT_PX // BLOCKSIZE
 
-    mine_coords = []
-
+    mine_rects = []
     for i in range(number_of_mines):
         rand_x = randint(0, grid_width) * 20
         rand_y = randint(0, grid_height) * 20
-        mine_coords.append((rand_x, rand_y))
+        minerect = pygame.Rect(rand_x, rand_y, BLOCKSIZE, BLOCKSIZE)
+        mine_rects.append(minerect)
 
-    return mine_coords
+    return mine_rects
 
 
 NUM_MINES = 100
@@ -78,21 +78,22 @@ MINES = get_mines(NUM_MINES)
 
 def get_adjacent(pressrect: pygame.Rect):
     """
-    Returns number of adjacent mines and rects of safe spaces
+    Returns rects of adjacent spaces
     """
     x = pressrect.x
     y = pressrect.y
     
     rects = []
-    num_mines = 0
 
     # This is really messy.
     # TODO @AlexisComix 
     # for some reason, None of the rects adjacent are in mines somehow.
     # No clue how this happens 
+    # The actual pressing nearby tiles works, but not mine count
     # up
+    # Fix? Separate mine counting and adjacent rect finding 
+
     rects.append(pygame.Rect(x, y-BLOCKSIZE, BLOCKSIZE, BLOCKSIZE))
-    print(pygame.Rect(x, y-BLOCKSIZE, BLOCKSIZE, BLOCKSIZE)) # TEST
     # down
     rects.append(pygame.Rect(x, y+BLOCKSIZE, BLOCKSIZE, BLOCKSIZE))
     # left
@@ -108,11 +109,21 @@ def get_adjacent(pressrect: pygame.Rect):
     # downleft
     rects.append(pygame.Rect(x-BLOCKSIZE, y+BLOCKSIZE, BLOCKSIZE, BLOCKSIZE))
 
-    for rect in rects:
-        if rect in MINES: 
-            num_mines += 1
+    return rects
 
-    return num_mines, rects
+
+def count_mines(rect: pygame.Rect) -> int:
+    """
+    Count mines in adjacent spaces to rect 'rect'
+    """
+    adjacent_tiles = get_adjacent(rect)
+    
+    count = 0
+    for tile in adjacent_tiles:
+        if tile in MINES:
+            count += 1
+
+    return count
 
 
 def main() -> None:
@@ -132,65 +143,60 @@ def main() -> None:
                     mouse_x, mouse_y = pygame.mouse.get_pos()
                     floor_x = floor(mouse_x / BLOCKSIZE) * BLOCKSIZE
                     floor_y = floor(mouse_y / BLOCKSIZE) * BLOCKSIZE
-                    mouse_grid_pos = (floor_x, floor_y)
+                    mouse_grid_rect = pygame.Rect(
+                        floor_x, floor_y, BLOCKSIZE, BLOCKSIZE
+                        )
 
-                    if mouse_grid_pos in MINES: # Mine detection
-                        print("BOOM!")          # Testing message
-                        mine_rect = pygame.Rect(
-                            mouse_grid_pos, (BLOCKSIZE, BLOCKSIZE)
-                            )
-                        print(mine_rect) # TEST
+                    if mouse_grid_rect in MINES:        # Mine detection
+                        mine_rect = mouse_grid_rect     # Abstraction
                         pygame.draw.rect(WIN, WHITE, mine_rect)
                         pygame.display.flip()
-                        pygame.time.wait(1000)  # Wait 1s
+                        pygame.time.wait(1000)          # Wait 1s
                         pygame.quit()
                         sys.exit()
                     else:
-                        click_rect = pygame.Rect(
-                            mouse_grid_pos, (BLOCKSIZE, BLOCKSIZE)
-                            )
+                        click_rect = mouse_grid_rect    # Abstraction
                         pressed.append(click_rect)
 
-                        _, adjacent = get_adjacent(click_rect)
-                        print(_)
+                        # Press adjacent tiles
+                        adjacent = get_adjacent(click_rect)
                         for square in adjacent:
                             pressed.append(square)
-
-        revealed = []
                         
         draw_grid()
-        for pressed_rect in pressed:
-            rect_x = pressed_rect.x
-            rect_y = pressed_rect.y
 
+        revealed = []
+        for pressed_rect in pressed:
             pygame.draw.rect(WIN, BLACK, pressed_rect)
             
-            mines, _ = get_adjacent(pressed_rect)
-            col = tuple()
-            if mines != 0:
-                match mines:
-                    case 1, 2:
-                        col = LIGHT_BLUE
-                    case 3, 4:
-                        col = BLUE
-                    case 5, 6:
-                        col = YELLOW
-                    case 7, 8:
-                        col = ORANGE
-                text = COMICSANSMS.render(f"{mines}", color=col)
-                revealed.append((text, click_rect))
-        
+            adj = get_adjacent(pressed_rect)
+            for tile in adj:
+                if tile not in MINES:
+                    mines = count_mines(tile)
+                    if mines != 0:
+                        col = WHITE # Default value to appease python
+                        if mines <= 2:
+                            col = LIGHT_BLUE
+                        elif mines <= 4:
+                            col = BLUE
+                        elif mines <= 6:
+                            col = YELLOW
+                        else:
+                            col = ORANGE
+
+                        text = COMICSANSMS.render(f"{mines}", False, col)
+                        revealed.append((text, tile))
+
+        for square in revealed:
+            txt = square[0]
+            rect = square[1]
+            WIN.blit(txt, rect)
+
         white_rect = pygame.Rect(
             0, GRID_HEIGHT_PX, 
             WIDTH, HEIGHT-GRID_HEIGHT_PX
             )
         pygame.draw.rect(WIN, WHITE, white_rect)
-
-        for square in revealed:
-            
-            txt = square[0]
-            rect = square[1]
-            WIN.blit(txt, rect)
 
         pygame.display.update()
         clock.tick(12)
